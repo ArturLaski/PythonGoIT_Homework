9aa489597faa9dcec991ac3a9d5f7757e6d1a1a1
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2024-03-17 20:24:39 +0100

    aktualizacja po poprawkach Mentora

--- TOPIC8/TASK_2.py
+++ TOPIC8/TASK_2.py
@@ -26,8 +26,7 @@ def show_phone(args, contacts):
     name = args[0]
     if name in contacts:
         return contacts[name]
-    else:
-        return "Contact not found."
+    return "Contact not found."
 
 def show_all(contacts):
     if not contacts:
